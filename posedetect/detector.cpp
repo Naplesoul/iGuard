b737bf9159dfd882756c5146d51967956b4af484
Naplesoul <shenwhang@outlook.com>
2022-05-14 18:49:23 +0800

    [improve]: add user parameters

--- posedetect/detector.cpp
+++ posedetect/detector.cpp
@@ -1,8 +1,17 @@
-#define OPENPOSE_FLAGS_DISABLE_POSE
-// #define OPENPOSE_FLAGS_DISABLE_DISPLAY
-#include <openpose/flags.hpp>
+#include <gflags/gflags.h>
 
 #include "detector.h"
+
+DEFINE_int32(fps_max, -1, "Max fps can run at, -1 means no limit.");
+
+DEFINE_bool(disable_multi_thread, false, "Whether to disable multi-thread.");
+DEFINE_bool(blend_original_frame, true, "Whether to blend original frame.");
+
+DEFINE_string(net_resolution, "-1x368", "Input resolution of the net, less can be faster and less accuracy, must be multiples of 16.");
+DEFINE_string(model_path, "../models",  "Path to caffe models directory.");
+DEFINE_string(pose_model, "BODY_25",    "Model to be used. `BODY_25` (fastest for CUDA version, most accurate, and includes"
+                                        " foot keypoints), `COCO` (18 keypoints), `MPI` (15 keypoints, least accurate model but"
+                                        " fastest on CPU), `MPI_4_layers` (15 keypoints, even faster but less accurate).");
 
 KeyPoint &Pose25::operator[](BodyPart bp)
 {
@@ -40,11 +49,16 @@ void Pose25::convert(std::vector<Pose25> &people,
     }
 }
 
-
-
 Detector::Detector(): opWrapper(op::ThreadManagerMode::Asynchronous)
 {
+    op::WrapperStructPose wrapperStructPose;
+    wrapperStructPose.netInputSize = op::flagsToPoint(op::String(FLAGS_net_resolution), "-1x368");
+    wrapperStructPose.poseModel = op::flagsToPoseModel(op::String(FLAGS_pose_model));
+    wrapperStructPose.blendOriginalFrame = FLAGS_blend_original_frame;
+    wrapperStructPose.modelFolder = op::String(FLAGS_model_path);
+    wrapperStructPose.fpsMax = FLAGS_fps_max;
 
+    opWrapper.configure(wrapperStructPose);
 }
 
 void Detector::start()
