9f513377ad6b9a195e47fd8460e964dee15fb9c7
Naplesoul <shenwhang@outlook.com>
2022-06-12 21:45:26 +0800

    [feat]: add coordinate conversion matrix

--- posedetect/camera.cpp
+++ posedetect/camera.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <eigen3/Eigen/LU>
+#include <eigen3/Eigen/Core>
 #include <opencv2/opencv.hpp>
 #include <librealsense2/rs.hpp>
 
@@ -26,13 +28,43 @@ std::vector<Camera> Camera::getCameras()
         cfg.enable_device(serial);
         cameras.emplace_back(cfg, ctx);
     }
+
+    return cameras;
 }
 
-void Camera::init(float _x, float _y, float _z)
+void Camera::init(float _w_ratio, float _x, float _y, float _z,
+                  float r_x_x, float r_x_y, float r_x_z,
+                  float r_y_x, float r_y_y, float r_y_z,
+                  float r_z_x, float r_z_y, float r_z_z)
 {
+    w_ratio = _w_ratio;
     x = _x;
     y = _y;
     z = _z;
+
+    Eigen::Matrix4f T_inv = Eigen::Matrix4f::Identity();
+    T_inv(0, 3) = x;
+    T_inv(1, 3) = y;
+    T_inv(2, 3) = z;
+
+    dir_x = Eigen::Vector3f(r_x_x, r_x_y, r_x_z);
+    dir_y = Eigen::Vector3f(r_y_x, r_y_y, r_y_z);
+    dir_z = Eigen::Vector3f(r_z_x, r_z_y, r_z_z);
+
+    dir_x.normalize();
+    dir_y.normalize();
+    dir_z.normalize();
+
+    Eigen::Matrix3f R3;
+    R3.row(0) = dir_x;
+    R3.row(1) = dir_y;
+    R3.row(2) = dir_z;
+
+    Eigen::Matrix3f R3_inv = R3.inverse();
+    Eigen::Matrix4f R4_inv = Eigen::Matrix4f::Identity();
+    R4_inv.block(0, 0, 3, 3) = R3_inv.block(0, 0, 3, 3);
+
+    Eigen::Matrix4f M_inv = T_inv * R4_inv;
 }
 
 void Camera::start()
@@ -58,8 +90,14 @@ KeyPoint3D Camera::convert3D(KeyPoint2D p)
 {
     float depth = dframe.get_distance(p.x, p.y);
 
-    KeyPoint3D p3d;
-    // TODO: calculate real-world position of p
+    Eigen::Vector3f cam_pos(p.x - w / 2, h / 2 - p.y, 0);
+    cam_pos = cam_pos * (w_ratio / w);
+    cam_pos(2) = 1;
+    cam_pos.normalize();
+    cam_pos = cam_pos * depth;
 
-    return p3d;
+    Eigen::Vector4f cam_pos_4(cam_pos(0), cam_pos(1), cam_pos(2), 1);
+    Eigen::Vector4f real_pos = M_inv * cam_pos_4;
+
+    return KeyPoint3D(real_pos(0), real_pos(1), real_pos(2), p.score);
 }
--- posedetect/camera.h
+++ posedetect/camera.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <eigen3/Eigen/Core>
 #include <opencv2/opencv.hpp>
 #include <librealsense2/rs.hpp>
 
@@ -10,6 +11,14 @@ class Camera
 {
 private:
     float x, y, z;
+    int w, h;
+
+    // assume a rectangle fills the picture when observed 1m away
+    // its width will be w_ratio, measured in meters
+    float w_ratio;
+
+    Eigen::Vector3f dir_x, dir_y, dir_z;
+    Eigen::Matrix4f M_inv;
 
     rs2::config cfg;
     rs2::context ctx;
@@ -23,7 +32,10 @@ public:
 
     static std::vector<Camera> getCameras();
 
-    void init(float x, float y, float z);
+    void init(float _w_ratio, float _x, float _y, float _z,
+              float r_x_x, float r_x_y, float r_x_z,
+              float r_y_x, float r_y_y, float r_y_z,
+              float r_z_x, float r_z_y, float r_z_z);
     void start();
     cv::Mat getFrame();
     KeyPoint3D convert3D(KeyPoint2D p);
