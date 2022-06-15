c2187e101f26b1108ea1b95d205f2d1dd23959cd
Naplesoul <shenwhang@outlook.com>
2022-06-15 16:48:36 +0800

    [feat]: add udp sender

---
+++ posedetect/nuitrack/src/udpsender.cpp
@@ -0,0 +1,50 @@
+#include "udpsender.h"
+
+#include <iostream>
+#include <jsoncpp/json/json.h>
+
+UDPSender::UDPSender(const char* addr, uint16_t port)
+{
+	addr_len = sizeof(struct sockaddr_in);
+	memset(&serveraddr, 0, addr_len);
+
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(port);
+	serveraddr.sin_addr.s_addr = inet_addr(addr);
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0)
+	{
+		perror("fail to setup socket");
+	}
+}
+
+UDPSender::~UDPSender()
+{
+    
+}
+
+void UDPSender::sendToServer(const char* buf, int len){
+    sendto(sockfd, buf, len, 0, (struct sockaddr*)&serveraddr, addr_len);
+}
+
+void UDPSender::sendPoseToServer(const tdv::nuitrack::Skeleton &skeleton)
+{
+	Json::Value json;
+	json["node_num"] = 24;
+	Json::Value nodes;
+	for (int i = 0; i < 24; ++i) {
+		Json::Value node;
+		node["x"] = skeleton.joints[i].real.x;
+		node["y"] = skeleton.joints[i].real.y;
+		node["z"] = skeleton.joints[i].real.z;
+        node["score"] = skeleton.joints[i].confidence;
+
+		nodes.append(node);
+	}
+	json["nodes"] = nodes;
+
+	std::cout << "\n\n\nSending\n" << json.toStyledString();
+	std::string jsonStr = Json::FastWriter().write(json);
+	sendToServer(jsonStr.c_str(), jsonStr.length());
+}
