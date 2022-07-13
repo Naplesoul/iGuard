7b9d400336fdff316182bdb9f6e834a9935dde65
Naplesoul <shenwhang@outlook.com>
2022-07-14 01:09:23 +0800

    [feat]: add combine all

--- combine/include/combine.h
+++ combine/include/combine.h
@@ -15,8 +15,6 @@ struct Node
     Node(): x(0), y(0), z(0), score(0) {}
     Node(int _x, int _y, int _z, int _score)
         : x(_x), y(_y), z(_z), score(_score) {}
-
-    void diff(const Node &ancher);
 };
 
 struct Skeleton
@@ -40,22 +38,25 @@ struct Skeleton
 class Combine
 {
 private:
-    float smooth;
     int mainCameraId = 1;
     int minorCameraId = 2;
     int handCameraId = 3;
-
-    struct sockaddr_in guiAddr;
-    struct sockaddr_in simAddr;
 
     int64_t mainFrameId = 0;
     int64_t minorFrameId = 0;
     int64_t handFrameId = 0;
     Skeleton skeleton;
 
-    void sendToGuiClient();
-    void sendToSimClient();
+    int sockfd;
+    const int addrLen = sizeof(struct sockaddr_in);
+    struct sockaddr_in guiAddr;
+    struct sockaddr_in simAddr;
 
+    Json::FastWriter writer;
+
+    void send();
+    void sendToGuiClient(const char *buf, int len);
+    void sendToSimClient(const char *buf, int len);
     void mainCameraRecv(const Json::Value &payload);
     void minorCameraRecv(const Json::Value &payload);
     void handCameraRecv(const Json::Value &payload);
