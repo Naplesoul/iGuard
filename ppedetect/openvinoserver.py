60732443400575b16835a039bacf0e40b7e76eb8
Naplesoul <shenwhang@outlook.com>
2022-07-24 12:31:41 +0800

    [feat]: send detect results

--- ppedetect/openvinoserver.py
+++ ppedetect/openvinoserver.py
@@ -1,3 +1,4 @@
+import json
 import time
 import socket
 import cv2
@@ -10,6 +11,7 @@ from augmentations import letterbox
 
 model_path = "./model/ppe.xml"
 class_names = ['glove', 'goggle', 'hand', 'head', 'helmet']
+colors = [(34, 139, 34), (46, 84, 8), (84, 46, 8), (0, 69, 255), (0, 0, 139)]
 
 
 def recv_img(recv: socket.socket):
@@ -64,6 +66,7 @@ if __name__ == "__main__":
                     height = int(h / ratio[1])
                     box = np.array([left, top, width, height])
                     boxes.append(box)
+        print("Detections: " + str(len(predictions)))
 
         indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45)
 
@@ -77,21 +80,30 @@ if __name__ == "__main__":
             filtered_boxes.append(boxes[i])
         print("Detections: " + str(len(filtered_ids)))
 
-        # colorbox
-        colors = [(46, 84, 8), (84, 46, 8), (0, 0, 139)]
+        has_glove = False
+        has_goggle = False
+        has_helmet = False
         # Show bbox
         for (class_id, confidence, box) in zip(filtered_ids, filered_confidences, filtered_boxes):
-            color = colors[int(class_id) % len(colors)]
+            id = int(class_id)
+            if id == 0:
+                has_glove = True
+            elif id == 1:
+                has_goggle = True
+            elif id == 4:
+                has_helmet = True
+            color = colors[id]
             cv2.rectangle(img, box, color, 3)
             cv2.rectangle(img, (box[0], box[1] - 30), (box[0] + box[2], box[1]), color, -1)
             cv2.putText(img, class_names[class_id], (box[0] + 20, box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-            print(class_names[class_id], box)
+            print(class_names[class_id], confidence, box)
             
         cv2.resize(img, (640, 360), img)
         success, buf = cv2.imencode(".jpg", img, [cv2.IMWRITE_JPEG_QUALITY, 10])
 
+        send.sendto(json.dumps({ "has_helmet": has_helmet, "has_goggle": has_goggle, "has_glove": has_goggle }).encode(), (config.server_ip, config.server_detect_port))
         send.sendto(buf, (config.server_ip, config.server_image_port))
 
         end_time = time.time()
-        print(f"Total process time: {int((end_time - start_time) * 1000)} ms.")
+        print(f"Total process time: {int((end_time - start_time) * 1000)} ms.\n\n")
         # cv2.imwrite("resize.png", img)
